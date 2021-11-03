d4b6969aaa129d17dd566a8c07f4f92ad6508869
Artem1KN <91216403+Artem1KN@users.noreply.github.com>
2021-11-03 21:45:02 +0500

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char stroka[255] = "";
+    cin >> stroka;
+    int big = 0, small = 0;
+    for (int i = 0; i < 255; i+=1)
+    {
+        if ((stroka[i] >= 'A') && (stroka[i] <= 'Z'))
+            big+=1;
+        if (stroka[i] >= 'a' && stroka[i] <= 'z')
+            small+=1;
+    }
+    cout << "Big " << big << endl;
+    cout << "Small " << small;
+    getwchar();
+    return 0;
+}
